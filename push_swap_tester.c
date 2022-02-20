032c8f25b83cda26aec1d85a7365aa430ca00251
Ismael Yahyaoui Racine <ismaelyahyaouiracine@Ismaels-MBP.lan>
2022-02-20 17:46:16 -0500

    Push_swap tester working

--- push_swap_tester.c
+++ push_swap_tester.c
@@ -2,22 +2,27 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#define TEMP_PATH "./ps_tester.tmp"
+#define LOG_5_PATH "./results_5.log"
+
 typedef struct	s_cmd {
 	int		in_fd;
 	int		out_fd;
 	pid_t	pid;
 }	t_cmd;
 
-void	assign_cmd_fds(t_cmd *ps, t_cmd *checker, t_cmd *wc, int log_fd);
+
 void 	execute_permutation(char *stack);
 void	exec_ps(t_cmd ps, char *stack);
 void	exec_checker(t_cmd ps, char *stack);
-void	exec_wc(t_cmd ps, int log_fd);
+void	exec_wc(t_cmd wc);
 void	tester_cleanup(t_cmd *ps, t_cmd *checker, t_cmd *wc, int log_fd);
+void	wait_cmds(pid_t pid_ps, pid_t pid_checker, pid_t pid_wc);
 
 int	main(void)
 {
 	char	*curr_line;
+	int		nl_pos;
 	int		fd;
 
 	fd = open("./5_permutations.txt", O_RDONLY);
@@ -25,8 +30,10 @@ int	main(void)
 	while (curr_line && ft_isdigit(curr_line[0]))
 	{
 		//Remove the '\n'
-		curr_line[ft_strlen(curr_line) - 1] = 0;
-		printf(MAGENTA"---> curr_line = '%s'\n"RESET_COL, curr_line);
+		nl_pos = strlen_c(curr_line, '\n');
+		if (nl_pos > 0)
+			curr_line[nl_pos - 1] = 0;
+		// printf(MAGENTA"---> curr_line = '%s'\n"RESET_COL, curr_line);
 		//Go to execute
 		execute_permutation(curr_line);
 		free (curr_line);
@@ -38,63 +45,101 @@ int	main(void)
 
 void	execute_permutation(char *stack)
 {
-	static t_cmd	ps;
-	static t_cmd	checker;
-	static t_cmd	wc;
+	t_cmd	ps;
+	t_cmd	checker;
+	t_cmd	wc;
 	char	*buff;
+	char	temp_fd;
 	int		log_fd;
+	int		status;
 
-	log_fd = open("./results_5.log", O_WRONLY | O_APPEND);
+	log_fd = open(LOG_5_PATH, O_WRONLY | O_APPEND);
+	if (log_fd == -1)
+		log_fd = open(LOG_5_PATH, O_WRONLY | O_APPEND | O_CREAT, 0644);
 	buff = ft_strjoin(stack, ": ");
 	
 	//Write the input in result.log
-	write(log_fd, buff, ft_strlen(buff) + 2);
+	write(log_fd, buff, ft_strlen(buff));
+	close(log_fd);
 	
-	//pipe ps and checker/wc
-	assign_cmd_fds(&ps, &checker, &wc, log_fd);
+	//Write everything to an intermediary file so that both checker and wc can
+	//easily access it
+	temp_fd = open(TEMP_PATH, O_CREAT | O_RDWR | O_APPEND, 0644);
 
 	//Execute push_swap
+	ps.out_fd = temp_fd;
 	ps.pid = fork();
 	if (ps.pid == 0)
 		exec_ps(ps, stack);
+	close(ps.out_fd);
+	close(temp_fd);
+
+	status = 0;
+	waitpid(ps.pid, &status, 0);
 
 	//Execute checker: will print to STDOUT, only for personal reference
+	temp_fd = open(TEMP_PATH, O_RDONLY, 0644);
+	printf(RED"temp_fd before exec_checker = %d\n"RESET_COL, temp_fd);
+	dup2(temp_fd, STDIN_FILENO);
+	// close(temp_fd);
+
+	log_fd = open(LOG_5_PATH, O_WRONLY | O_APPEND);
+	checker.out_fd = log_fd;
 	checker.pid = fork();
 	if (checker.pid == 0)
 		exec_checker(checker, stack);
+	close(checker.out_fd);
+	close(temp_fd);
+
+	waitpid(checker.pid, &status, 0);
 
 	//Execute wc: output will be redirected to results.log
+	temp_fd = open(TEMP_PATH, O_RDONLY, 0644);
+	printf(RED"temp_fd before exec_wc = %d\n"RESET_COL, temp_fd);
+	dup2(temp_fd, STDIN_FILENO);
+	close(temp_fd);
+	log_fd = open(LOG_5_PATH, O_WRONLY | O_APPEND);
+	wc.out_fd = log_fd;
 	wc.pid = fork();
 	if (wc.pid == 0)
-		exec_wc(wc, log_fd);
+		exec_wc(wc);
+	close(wc.out_fd);
+	close(temp_fd);
 
 	tester_cleanup(&ps, &checker, &wc, log_fd);
+	waitpid(wc.pid, &status, 0);
+	
+	write(log_fd, "\n", 1);
 
-	close(log_fd);
+}
+
+void	wait_cmds(pid_t pid_ps, pid_t pid_checker, pid_t pid_wc)
+{
+	int	status;
+
+	status = 0;
+	printf(YELLOW"waiting for PID_ps : %d\n"RESET_COL, pid_ps);
+	waitpid(pid_ps, &status, 0);
+	printf(YELLOW"waiting for PID_wc : %d\n"RESET_COL, pid_wc);
+	waitpid(pid_wc, &status, 0);
+	printf(YELLOW"waiting for PID_checker : %d\n"RESET_COL, pid_checker);
+	waitpid(pid_checker, &status, 0);
+
+	sleep(1);
 }
 
 void	tester_cleanup(t_cmd *ps, t_cmd *checker, t_cmd *wc, int log_fd)
 {
 	close(log_fd);
-	close(ps->out_fd);
+	close(ps->in_fd);
 	close(checker->in_fd);
 	close(wc->in_fd);
-	ps->out_fd = 0;
-	checker->in_fd = 0;
-	wc->in_fd = 0;
+	close(ps->out_fd);
+	close(checker->out_fd);
+	close(wc->out_fd);
+	unlink(TEMP_PATH);
 }
 
-void	assign_cmd_fds(t_cmd *ps, t_cmd *checker, t_cmd *wc, int log_fd)
-{
-	int	pipe_fds[2];
-
-	if (pipe(pipe_fds) == -1)
-		exit(errno);
-	ps->out_fd = pipe_fds[1];
-	checker->in_fd = pipe_fds[0];
-	wc->in_fd = pipe_fds[0];
-	return ;
-}
 
 void	exec_ps(t_cmd ps, char *stack)
 {
@@ -111,21 +156,19 @@ void	exec_checker(t_cmd checker, char *stack)
 {
 	char	*argv[] = {"./checker_Mac", stack, NULL};
 
-	dup2(checker.in_fd, STDIN_FILENO);
-	close(checker.in_fd);
+	dup2(checker.out_fd, STDOUT_FILENO);
+	close(checker.out_fd);
 	execv(argv[0], argv);
 	perror("exec_checker failed");
 	exit(errno);
 }
 
-void	exec_wc(t_cmd wc, int log_fd)
+void	exec_wc(t_cmd wc)
 {
 	char	*argv[] = {"/usr/bin/wc", "-l", NULL};
 
-	dup2(wc.in_fd, STDIN_FILENO);
-	close(wc.in_fd);
-	dup2(log_fd, STDOUT_FILENO);
-	close(log_fd);
+	dup2(wc.out_fd, STDOUT_FILENO);
+	close(wc.out_fd);
 	execv(argv[0], argv);
 	perror("exec_wc failed");
 	exit(errno);
