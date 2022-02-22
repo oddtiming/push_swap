b7ddab2e65733947b8bf8418755b6b5537b2b0f2
Ismael Yahyaoui Racine <ismaelyahyaouiracine@Ismaels-MBP.lan>
2022-02-21 23:21:18 -0500

    Okay, made a working version again...

--- incl/push_swap.h
+++ incl/push_swap.h
@@ -40,7 +40,7 @@ typedef struct s_stacks
 	int	size;
 	int	sizeA;
 	int	sizeB;
-}	t_stacks;
+}	t_main_container;
 
 typedef enum e_moves
 {
@@ -59,21 +59,25 @@ typedef enum e_moves
 
 //Functions
 //PARSING
-void	parse(int argc, char *argv[], t_stacks *cont);
-void	assign_inputs(char **args, t_stacks *cont, int size);
+void	parse(int argc, char *argv[], t_main_container *cont);
+void	assign_inputs(char **args, t_main_container *cont, int size);
 int		*normalize_stack_values(int *stack, int size);
 
 
 //MOVES
-void	rotate(int *stack, int size, bool is_reverse);
+void	rotate(int *stack, int size);
+void	rev_rotate(int *stack, int size);
 void	swap(int *stack, int size);
 void	push(int *stack_src, int *stack_dest, int *size_src, int *size_dest);
-void	make_move(t_stacks *cont, int move);
+void	make_rab(int *stack, int size, int move);
+void	make_rrab(int *stack, int size, int move);
+void	make_sab(int *stack, int size, int move);
+void	make_push(t_main_container *cont, int move);
 
 //SOLVING
-void	sort(t_stacks *cont);
-void	sort_3(t_stacks *cont);
-void	sort_5(t_stacks *cont);
+void	sort(t_main_container *cont);
+void	sort_3(t_main_container *cont);
+void	sort_5(t_main_container *cont);
 int		get_pos_smallest(int *stack, int size);
 int		get_smallest_val(int *stack, int size);
 int		get_biggest_val(int *stack, int size);
@@ -83,12 +87,12 @@ int		nb_sorted_at_pos(int *stack, int size, int pos);
 void	exit_on_err(char *err_message);
 
 //UTILS
-void	print_stacks(t_stacks *cont);
+void	print_stacks(t_main_container *cont);
 void	print_single_stack(int *stack, int size);
 void	print_move(int move);
 void	set_next_index(int *pos, int size);
 
 //CLEANUP
-void	cleanup(t_stacks *cont);
+void	cleanup(t_main_container *cont);
 
 #endif
---
+++ src/sort_5.c
@@ -0,0 +1,176 @@
+#include "push_swap.h"
+
+void	rotate_to_pos0(int *stack, int size, int pos)
+{
+	if (pos < size - pos)
+		while (pos-- > 0)
+			make_rab(stack, size, RA);
+	else
+		while (pos++ < size)
+			make_rrab(stack, size, RRA);
+	if (DEBUG)
+	{
+		printf(YELLOW"--------AFTER rotate_to_pos0--------\n"RESET_COL);
+		print_single_stack(stack, size);
+	}
+	return ;
+}
+
+bool	try_pb(t_main_container *cont)
+{
+	int	pos_in_stack;
+	int	stack_head;
+
+	pos_in_stack = 0;
+	while (pos_in_stack < cont->sizeA)
+	{
+		push(cont->A, cont->B, &cont->sizeA, &cont->sizeB);
+		stack_head = get_pos_smallest(cont->A, cont->sizeA);
+		if (nb_sorted_at_pos(cont->A, cont->sizeA, stack_head) == cont->sizeA)
+		{}
+
+
+	}
+	return (false);
+}
+
+bool	try_swap(t_main_container *cont)
+{
+	int *stack;
+	int size;
+	int	pos;
+	int	stack_head;
+
+	stack = cont->A;
+	size = cont->sizeA;
+	pos = 0;
+	while (pos < size - 1)
+	{
+		ft_swap_ints(&stack[pos], &stack[pos + 1]);
+		stack_head = get_pos_smallest(stack, size);
+		if (nb_sorted_at_pos(stack, size, stack_head) == size)
+		{
+			ft_swap_ints(&stack[pos], &stack[pos + 1]);
+			rotate_to_pos0(cont->A, cont->sizeA, pos);
+			make_sab(cont->A, cont->sizeA, SA);
+			return (true);
+		}
+		else
+			ft_swap_ints(&stack[pos], &stack[pos + 1]);
+		pos++;
+	}
+	if (DEBUG)
+	{
+		printf(YELLOW"--------AFTER try_swap--------\n"RESET_COL);
+		print_stacks(cont);
+	}
+	return (false);
+}
+
+bool	try_rotate(int *stack, int size)
+{
+	int	stack_head;
+
+	stack_head = get_pos_smallest(stack, size);
+	if (nb_sorted_at_pos(stack, size, stack_head) == size)
+	{
+		rotate_to_pos0(stack, size, stack_head);
+		return (true);
+	}
+	return (false);
+}
+
+void	insert_b(t_main_container *cont)
+{
+	int	pos_of_b0_in_a;
+	int	insertion_val;
+
+	while (cont->sizeB)
+	{
+		if (cont->B[0] < cont->size - 1)
+			insertion_val = cont->B[0] + 1;
+		else
+			insertion_val = 0;
+		if (DEBUG)
+			printf(RED"------>in insert_b, insertion_val of %d = %d\n"RESET_COL, cont->B[0], insertion_val);
+		pos_of_b0_in_a = 0;
+		while (cont->A[pos_of_b0_in_a] != insertion_val)
+		{
+			set_next_index(&pos_of_b0_in_a, cont->sizeA);
+			if (pos_of_b0_in_a == 0)
+				set_next_index(&insertion_val, cont->size);
+		}	
+		if (DEBUG)
+			printf(RED"------>in insert_b, pos_of %d in a = %d\n"RESET_COL, insertion_val, pos_of_b0_in_a);
+		rotate_to_pos0(cont->A, cont->sizeA, pos_of_b0_in_a);
+		make_push(cont, PA);
+	}
+	return ;
+}
+
+bool	try_rotate_and_swap(t_main_container *cont)
+{
+	int stack_head;
+
+	stack_head = get_pos_smallest(cont->A, cont->sizeA);
+	if (nb_sorted_at_pos(cont->A, cont->sizeA, stack_head) == cont->sizeA)
+	{
+		if (cont->sizeA != cont->size)
+			insert_b(cont);
+		stack_head = get_pos_smallest(cont->A, cont->sizeA);
+		if (DEBUG)
+		{
+			printf(RED"#############stack_head = %d\n"RESET_COL, stack_head);
+			printf(RED"#############cont->A[stack_head] = %d\n"RESET_COL, cont->A[stack_head]);
+		}
+		rotate_to_pos0(cont->A, cont->sizeA, stack_head);
+		return (true);
+	}
+	else if (try_swap(cont) == true)
+	{
+		if (cont->sizeA != cont->size)
+			insert_b(cont);
+		stack_head = get_pos_smallest(cont->A, cont->sizeA);
+		if (DEBUG)
+		{
+			printf(RED"#############stack_head = %d\n"RESET_COL, stack_head);
+			printf(RED"#############cont->A[stack_head] = %d\n"RESET_COL, cont->A[stack_head]);
+		}
+		rotate_to_pos0(cont->A, cont->sizeA, stack_head);
+		return (true);
+	}
+	return (false);
+}
+
+//Need to code try_pb (which will try ALL positions in stack A to see which
+//is closest to pos_0) to try to see if pushing a single value can allow
+//try_swap to sort the 4 remaining ones.
+//I believe that the only case that would result in more than 7 moves with this
+//method is "4 3 2 1 0" (8 moves). The only way I found to make it in less is :
+//	-pb; sa-> 2 3 1 0 | 4 -ra x2; sa-> 0 1 2 3 | 4 -pa; ra-> [DONE] (7 moves)
+
+void	sort_5(t_main_container *cont)
+{
+	if (try_rotate_and_swap(cont) == true)
+	{
+		if (DEBUG)
+		{
+			printf("--------SOLVED BOARD--------");
+			print_stacks(cont);
+		}
+		return ;
+	}
+	else
+	{
+		// if (try_pb == true)
+		// 	return ;
+		make_push(cont, PB);
+		if (DEBUG)
+		{
+			printf(YELLOW"--------AFTER pb--------\n"RESET_COL);
+			print_stacks(cont);
+		}
+		sort_5(cont);
+	}
+	return ;
+}
