9a5480552bc1e31aeb79a73782d873c72b81928c
Ismael Yahyaoui Racine <ismaelyahyaouiracine@Ismaels-MBP.lan>
2022-03-05 15:25:31 -0500

    Slightly improved tester
    - Displays color of nb_moves based on whether or not it's above limit,
    - Displays color for final results of average and max nb_moves, as well as nb_above_limit
    - Doesn't display duplicate if val > max || val > limit
    - Still logs the max_moves if no value is above limit
    - If many values are above limit, they will all be logged

--- src/sort.c
+++ src/sort.c
@@ -61,11 +61,16 @@ void	sort(t_main_cont *cont)
 		sort_small(cont);
 	else
 	{
-		while (cont->stack_a.size != 6)
+		while (cont->stack_a.size != 5)
 		{
-			do_pb(cont, &cont->curr_moves);
+			do_pb(cont, &cont->final_moves);
 		}
+
+		// if (!is_sorted(&cont->stack_a, cont->head_a.index))
+		// 	do_sa(cont, &cont->final_moves);
+		// insert_b(cont, &cont->final_moves);
 		sort_small(cont);
+		rotate_to_0_in_a(cont, &cont->final_moves, cont->head_a.index);
 	}
 
 	return ;
--- tester.sh
+++ tester.sh
@@ -4,12 +4,14 @@ RED="\033[31m"
 GREEN="\033[32m"
 ENDCOLOR="\033[0m"
 
-NB_VALS=8
+NB_VALS=100
 MAX_NB_MOVES=0
+MAX_TEST_ID=0
 N=1.38
 LIMIT=$(bc -l <<< "e($N*l($NB_VALS))")
-NB_CASES_ABOVE_MAX=0
-ITERATIONS=0
+INT_LIMIT=${LIMIT%.1}
+NB_CASES_ABOVE_LIMIT=0
+ITERATIONS=1
 SUM=0
 LOG_FILE=sore_thumbs_$NB_VALS.log
 PS_TEMPFILE=tester_temp.log
@@ -17,7 +19,7 @@ PS_TEMPFILE=tester_temp.log
 echo -e "${GREEN}Testing for ${NB_VALS} values ${ENDCOLOR}"
 
 
-for i in {1..10}
+for i in {1..5}
 do
 	export ARG=`ruby -e "puts (1..$NB_VALS).to_a.shuffle.join(' ')"`
 	./push_swap $ARG > $PS_TEMPFILE
@@ -31,32 +33,58 @@ do
 		echo $NB_MOVES >> $LOG_FILE
 		break
 	fi
-	if [ $(echo "$NB_VALS > $LIMIT" | bc) -eq 1 ];
-		then
-		echo $ARG >> $LOG_FILE
-		cat $PS_TEMPFILE | ./checker_Mac $ARG
-		echo "nb_moves:" >> $LOG_FILE
-		echo $NB_MOVES >> $LOG_FILE
-		let NB_CASES_ABOVE_MAX+=1;
-		echo $ARG 
-	fi
-	if [ $(echo $NB_MOVES ">" $MAX_NB_MOVES | bc) -eq 1 ]
+	if [ $(echo "$NB_MOVES > $LIMIT" | bc) -eq 1 ];
 	then
-		MAX_NB_MOVES=$NB_MOVES;
-		MAX_ARGS=$ARGS;
+		echo "Test #"$i":" $NB_MOVES "[$(cat $PS_TEMPFILE | ./checker_Mac $ARG)]" >> $LOG_FILE
+		echo $ARG >> $LOG_FILE
+		echo "" >> $LOG_FILE
+		let NB_CASES_ABOVE_LIMIT+=1;
+		echo $ARG 
+		else if [ $(echo $NB_MOVES ">" $MAX_NB_MOVES | bc) -eq 1 ]
+		then
+			echo "Test #"$i":" $NB_MOVES "[$(cat $PS_TEMPFILE | ./checker_Mac $ARG)]" >> $LOG_FILE
+			echo "$ARG" >> $LOG_FILE
+			echo "" >> $LOG_FILE
+			MAX_NB_MOVES=$NB_MOVES;
+			MAX_TEST_ID=$i;
+		fi
 	fi
-	echo $i ":" $NB_MOVES
+
+	if [ $(echo $NB_MOVES "<" $LIMIT | bc) -eq 1 ];
+	then
+		echo -e "${i} : ${GREEN}${NB_MOVES}${ENDCOLOR}"
+	else
+		echo -e "${i} : ${RED}${NB_MOVES}${ENDCOLOR}"
+	fi
 	let SUM+=$NB_MOVES;
 	let ITERATIONS+=1;
 	let i+=1;
 done
 
 rm $PS_TEMPFILE
-echo "Nb tests done:" $ITERATIONS
-echo "Average nb of moves: $(($SUM / $ITERATIONS))"
-echo "Worst nb of moves: "$MAX_NB_MOVES
-echo "Nb of moves above" $LIMIT":" $NB_CASES_ABOVE_MAX
-echo "Worst nb of moves: "$MAX_NB_MOVES >> $LOG_FILE
-echo "Arguments:\n "$MAX_ARGS >> $LOG_FILE
+echo "Nb tests done:" $(($ITERATIONS - 1))
+if [ $(echo "$(($SUM / ($ITERATIONS - 1))) < ${LIMIT}" | bc) -eq 1 ];
+then
+	echo -e "${GREEN}Average nb of moves: $(($SUM / ($ITERATIONS - 1)))${ENDCOLOR}"
+else
+	echo -e "${RED}Average nb of moves: $(($SUM / ($ITERATIONS - 1)))${ENDCOLOR}"
+fi
+
+if [ $(echo $MAX_NB_MOVES "<" $LIMIT | bc) -eq 1 ];
+then
+	echo -e "${GREEN}Worst nb of moves: ${MAX_NB_MOVES}${ENDCOLOR}"
+else
+	echo -e "${RED}Worst nb of moves: ${MAX_NB_MOVES}${ENDCOLOR}"
+fi
+
+if [ $NB_CASES_ABOVE_LIMIT -eq 0 ];
+then
+	printf "${GREEN}%s %.0f %s %d\n${ENDCOLOR}" "Nb of moves above" $LIMIT ":" $NB_CASES_ABOVE_LIMIT
+else
+	printf "${RED}%s %.0f %s %d\n${ENDCOLOR}" "Nb of moves above" $LIMIT ":" $NB_CASES_ABOVE_LIMIT
+fi
+
 echo "Nb tests done:" $ITERATIONS >> $LOG_FILE
-echo "Nb of moves above" $LIMIT":" $NB_CASES_ABOVE_MAX >> $LOG_FILE
+echo "Average nb of moves: $(($SUM / ($ITERATIONS - 1)))" >> $LOG_FILE
+echo "(Test #"$MAX_TEST_ID") Worst nb of moves:" $MAX_NB_MOVES >> $LOG_FILE
+printf "%s %.0f %s %d\n" "Nb of moves above" $LIMIT ":" $NB_CASES_ABOVE_LIMIT >> $LOG_FILE
