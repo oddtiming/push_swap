d84566ca51799e8935c3d1da91f6077f7c117bf6
oddtiming <36460419+oddtiming@users.noreply.github.com>
2022-03-25 18:38:32 -0400

    Updated submodule

--- src/utils.c
+++ src/utils.c
@@ -13,26 +13,6 @@ void	*ft_safealloc(size_t size)
 	return (mem_space);
 }
 
-int ft_max(int a, int b)
-{
-	return ((a >= b) * a + (b > a) * b);
-}
-
-int ft_min(int a, int b)
-{
-	return ((a <= b) * a + (b < a) * b);
-}
-
-int ft_abs(int a)
-{
-	return ((a >= 0) * a + (a < 0) * (-a));
-}
-
-bool	ft_same_sign(int a, int b)
-{
-	return ((a >= 0 && b >= 0) || ((a < 0) && (b < 0)));
-}
-
 void	set_next_pos(int *pos, int size)
 {
 	if (*pos == size - 1)
