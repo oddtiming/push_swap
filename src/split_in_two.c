f626a8b584e1cfe3bd8ab6d1593f590b0722290a
oddtiming <36460419+oddtiming@users.noreply.github.com>
2022-04-20 20:10:01 -0400

    Fixed split_in_two
    I think I might have mistaken an int and a float, which would effectively render useless the whole algo...

--- src/split_in_two.c
+++ src/split_in_two.c
@@ -17,7 +17,8 @@ int	max_elem_leaving(t_main_cont *cont, t_deque *leaving_vals)
 	return (max_elem);
 }
 
-void	try_single_cutoff(t_main_cont *cont, t_deque *block_ids, float cutoff)
+static void	try_single_cutoff(
+	t_main_cont *cont, t_deque *block_ids, float cutoff)
 {
 	partition_leaving_vals_cutoff(
 		&cont->leaving_vals,
@@ -35,10 +36,68 @@ void	try_single_cutoff(t_main_cont *cont, t_deque *block_ids, float cutoff)
 	return ;
 }
 
+static void	try_multiple_cutoffs_body(
+	t_main_cont *cont, t_deque *block_ids, float cutoff, int id_to_push)
+{
+	int	i;
+
+	partition_leaving_vals_cutoff(
+		&cont->leaving_vals,
+		block_ids,
+		(int)(max_elem_leaving(cont, &cont->leaving_vals) * cutoff));
+	insert_block_of_a_in_b(cont, block_ids, id_to_push);
+	deque_reinit_list(&cont->leaving_vals);
+	i = 0;
+	while (i < cont->stack_a.size)
+	{
+		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
+			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
+		else
+			cont->leaving_vals.add_last(
+				&cont->leaving_vals,
+				cont->stack_a.elems[i]);
+		i++;
+	}
+}
+
+static void	try_multiple_cutoffs_footer(
+	t_main_cont *cont, t_deque *block_ids, float cutoff, int id_to_push)
+{
+	int	i;
+
+	partition_leaving_vals_cutoff(
+		&cont->leaving_vals, block_ids,
+		(int)(max_elem_leaving(cont, &cont->leaving_vals) * cutoff));
+	insert_block_of_a_in_b(cont, block_ids, id_to_push);
+	insert_block_of_a_in_b(cont, block_ids, (id_to_push + 1) % 2);
+	insert_b(cont, &cont->curr_moves);
+	rotate_to_0_in_a(cont, &cont->curr_moves, cont->head_a.index);
+	if (cont->curr_moves.size < cont->best_moves.size)
+		copy_deque(&cont->curr_moves, &cont->best_moves);
+	copy_deque(&cont->initial_stack, &cont->stack_a);
+	deque_reinit_list(&cont->stack_b);
+	deque_reinit_list(&cont->curr_moves);
+	deque_reinit_list(&cont->leaving_vals);
+	i = 0;
+	while (i < cont->stack_a.size)
+	{
+		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
+			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
+		else
+			cont->leaving_vals.add_last(
+				&cont->leaving_vals, cont->stack_a.elems[i]);
+		i++;
+	}	
+}
+
+static void	split_in_two_mult_cutoffs(t_main_cont *cont, t_deque *block_ids)
+{
+
+}
+
 void	split_in_two(t_main_cont *cont)
 {
 	t_deque	*block_ids;
-	int		i;
 
 	new_deque(&block_ids);
 
@@ -48,264 +107,38 @@ void	split_in_two(t_main_cont *cont)
 	try_single_cutoff(cont, block_ids, 3 / 8);
 	try_single_cutoff(cont, block_ids, 7 / 24);
 
+	try_multiple_cutoffs_body(cont, block_ids, 0.6F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.4F, 1);
 
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 3 / 5);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 2 / 5);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	insert_b(cont, &cont->curr_moves);
-	rotate_to_0_in_a(cont, &cont->curr_moves, cont->head_a.index);
-	if (cont->curr_moves.size < cont->best_moves.size)
-	{
-		copy_deque(&cont->curr_moves, &cont->best_moves);
-	}
-		// undo_moves(cont, &cont->curr_moves);
-		copy_deque(&cont->initial_stack, &cont->stack_a);
-		deque_reinit_list(&cont->stack_b);
-	deque_reinit_list(&cont->curr_moves);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
+	try_multiple_cutoffs_body(cont, block_ids, 0.6F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.4F, 1);
 
+	try_multiple_cutoffs_body(cont, block_ids, 0.6F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.4F, 1);
 
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 3 / 5);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 2 / 5);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	insert_b(cont, &cont->curr_moves);
-	rotate_to_0_in_a(cont, &cont->curr_moves, cont->head_a.index);
-	if (cont->curr_moves.size < cont->best_moves.size)
-	{
-		copy_deque(&cont->curr_moves, &cont->best_moves);
-	}
-		// undo_moves(cont, &cont->curr_moves);
-		copy_deque(&cont->initial_stack, &cont->stack_a);
-		deque_reinit_list(&cont->stack_b);
-	deque_reinit_list(&cont->curr_moves);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
+	try_multiple_cutoffs_body(cont, block_ids, 0.6F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.4F, 0);
 
+	try_multiple_cutoffs_body(cont, block_ids, 0.6F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.4F, 0);
 
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 3 / 5);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 2 / 5);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	insert_b(cont, &cont->curr_moves);
-	rotate_to_0_in_a(cont, &cont->curr_moves, cont->head_a.index);
-	if (cont->curr_moves.size < cont->best_moves.size)
-	{
-		copy_deque(&cont->curr_moves, &cont->best_moves);
-	}
-		copy_deque(&cont->initial_stack, &cont->stack_a);
-		deque_reinit_list(&cont->stack_b);
-	deque_reinit_list(&cont->curr_moves);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
+	try_multiple_cutoffs_body(cont, block_ids, 0.6F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.4F, 0);
 
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 1);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.5F, 1);
 
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	insert_b(cont, &cont->curr_moves);
-	rotate_to_0_in_a(cont, &cont->curr_moves, cont->head_a.index);
-	if (cont->curr_moves.size < cont->best_moves.size)
-	{
-		copy_deque(&cont->curr_moves, &cont->best_moves);
-	}
-		// undo_moves(cont, &cont->curr_moves);
-		copy_deque(&cont->initial_stack, &cont->stack_a);
-		deque_reinit_list(&cont->stack_b);
-	deque_reinit_list(&cont->curr_moves);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 0);
+	try_multiple_cutoffs_body(cont, block_ids, 0.5F, 0);
+	try_multiple_cutoffs_footer(cont, block_ids, 0.5F, 0);
 
-
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
-	partition_leaving_vals_cutoff(
-		&cont->leaving_vals, block_ids, max_elem_leaving(cont, &cont->leaving_vals) * 1 / 2);
-	insert_block_of_a_in_b(cont, block_ids, 1);
-	insert_block_of_a_in_b(cont, block_ids, 0);
-	insert_b(cont, &cont->curr_moves);
-	rotate_to_0_in_a(cont, &cont->curr_moves, cont->head_a.index);
-	if (cont->curr_moves.size < cont->best_moves.size)
-	{
-		copy_deque(&cont->curr_moves, &cont->best_moves);
-	}
-		// undo_moves(cont, &cont->curr_moves);
-		copy_deque(&cont->initial_stack, &cont->stack_a);
-		deque_reinit_list(&cont->stack_b);
-	deque_reinit_list(&cont->curr_moves);
-	deque_reinit_list(&cont->leaving_vals);
-	i = 0;
-	while (i < cont->stack_a.size)
-	{
-		if (is_in_stack(&cont->staying_vals, cont->stack_a.elems[i]))
-			cont->leaving_vals.add_last(&cont->leaving_vals, -1);
-		else
-			cont->leaving_vals.add_last(&cont->leaving_vals, cont->stack_a.elems[i]);
-		i++;
-	}
 
 	// Free block_ids
 	block_ids->free_list(block_ids);
