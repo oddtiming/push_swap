066fbdfc97797e668d7e987b1fdaab18a8f75fc6
Ismael Yahyaoui Racine <iyahoui-@c2r5p8.42quebec.com>
2022-04-25 15:39:22 -0400

    Updated submodule

--- src/checker_bonus.c
+++ src/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: iyahoui- <iyahoui-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 01:02:06 by iyahoui-          #+#    #+#             */
-/*   Updated: 2022/04/21 13:00:44 by iyahoui-         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:29:04 by iyahoui-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	set_moves_pointers(void (**array)(t_checker *))
 	array[RA_INDEX] = checker_ra;
 	array[RB_INDEX] = checker_rb;
 	array[RR_INDEX] = checker_rr;
-	array[RRA_INDEX] = &checker_rra;
-	array[RRB_INDEX] = &checker_rrb;
-	array[RRR_INDEX] = &checker_rrr;
+	array[RRA_INDEX] = checker_rra;
+	array[RRB_INDEX] = checker_rrb;
+	array[RRR_INDEX] = checker_rrr;
 	array[PA_INDEX] = checker_pa;
 	array[PB_INDEX] = checker_pb;
 	return ;
@@ -42,35 +42,49 @@ void	init_bonus(t_checker *checker, char **args)
 	return ;
 }
 
+bool	is_a_move(char *curr_line)
+{
+	if (!ft_strcmp(curr_line, "rr") || !ft_strcmp(curr_line, "ra") || !ft_strcmp(curr_line, "rb")
+		|| !ft_strcmp(curr_line, "rrr") || !ft_strcmp(curr_line, "rra\n") || !ft_strcmp(curr_line, "rrb")
+		|| !ft_strcmp(curr_line, "ss") || !ft_strcmp(curr_line, "sa") || !ft_strcmp(curr_line, "sb")
+		|| !ft_strcmp(curr_line, "pa") || !ft_strcmp(curr_line, "pb"))
+		return (true);
+	return (false);
+}
+
 void	do_moves(t_checker *checker)
 {
 	int		move;
-	int		pos;
-	char	c;
+	int		i;
+	char	*curr_line;
 
-	pos = 0;
-	move = 0;
-	while (read(STDIN_FILENO, &c, 1))
+	curr_line = get_next_line(STDIN_FILENO);
+	write(1, "entered do_moves\n", 17);
+	printf("entered do_moves (printf)\n");
+	while (curr_line)
 	{
-		pos++;
-		move = (move << 8) | c;
-		if ((pos == 2 && (c == 'a' || c == 'b' || c == 's' || c == 'r'))
-			|| (pos == 3 && (c != 'r' || c != 's' || c != 'a' || c != 'b')))
+		write(1, "entered do_moves\n", 17);
+		if (!is_a_move(curr_line))
 		{
-			checker->moves_fcts[convert_move_to_index(move)](checker);
-			pos = 0;
-			move = 0;
+			ft_putstr_fd("Error\n", 1);
+			free(curr_line);
+			return ;
 		}
-		if ((pos == 1 && (c == 'r' || c == 'p' || c == 's'))
-			|| (pos == 2 && (c == 'r'|| c == 'a' || c == 'b'))
-			|| (pos == 3 && (c != 'r' || c == 's' || c == 'a' || c == 'b' || c == '\n' || !c)))
-			continue;
 		else
 		{
-			free_checker(checker);
-			exit_on_err("Error\n");
+			move = 0;
+			i = 0;
+			while (curr_line[i] && curr_line[i] != '\n')
+			{
+				move |= (move << 8) | curr_line[i];
+				i++;
+			}
+			checker->moves_fcts[convert_move_to_index(move)](checker);
 		}
+		free(curr_line);
+		curr_line = get_next_line(STDIN_FILENO);
 	}
+	return ;
 }
 
 void	free_checker(t_checker *checker)
@@ -93,16 +107,22 @@ int	main(int argc, char *argv[])
 	parse_bonus(checker, argc, argv);
 	do_moves(checker);
 	i = 0;
+	printf("back in main\n");
 	while (++i < checker->stack_a.size)
 	{
-		if (checker->stack_a.elems[i] > checker->stack_a.elems[i] - 1)
+		if (checker->stack_a.elems[i] < checker->stack_a.elems[i - 1])
 		{
-			write(STDIN_FILENO, "KO\n", 3);
-			free_checker(checker);
-			return (1);
+			printf("checker found to not be sorted\n");
+			printf("\n");
+			write(STDOUT_FILENO, "KO\n", 3);
+			break ;
 		}
 	}
-	write(STDIN_FILENO, "OK\n", 3);
+	if (i == checker->stack_a.size)
+	{
+		printf("checker found to be sorted");
+		write(STDOUT_FILENO, "OK\n", 3);
+	}
 	free_checker(checker);
 	return (0);
 }
